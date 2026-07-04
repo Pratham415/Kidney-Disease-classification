dbb9bd751936896459980625f6c3496deac19e85
Pratham415 <pratham0401patel@email.com>
2026-07-04 11:43:56 +0530

    utils added

--- template.py
+++ template.py
@@ -4,7 +4,7 @@ import logging
 
 #logging string 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-project_name = "CNN-Classifier"
+project_name = "cnnClassifier"
 list_of_files = [
     ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
