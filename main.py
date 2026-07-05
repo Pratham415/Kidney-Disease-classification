e711c69a7e4e3f63386e8be6fd9efe68634e2b99
Pratham415 <pratham0401patel@email.com>
2026-07-05 18:02:29 +0530

    data ingestion done

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-from src.cnnClassifier import logger
-logger.info("Starting the CNN Classifier application...")
+from cnnClassifier import logging
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionPipeline
+
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+    logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionPipeline()
+    obj.main()
+    logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logging.exception(e)
+    raise e
